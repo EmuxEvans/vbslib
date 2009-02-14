f01969e72e0d21f280383a7c6ee6aa97d7474b14
toki <toki@534a8646-05ba-11de-b5bf-434079a3519f>
2009-02-14 19:00:18 +0000

    *** empty log message ***

--- trunk/test/run.bat
+++ trunk/test/run.bat
@@ -1,2 +1,9 @@
 @echo off
-dir /b test_*.vbs | cscript %~dp0..\bin\TestRunner.wsf //Job:ConsoleTestRunner /stdin+ %*
+
+cmd /v
+
+setlocal
+set TEST_LIST=
+for %%t in (test_*.vbs) do set TEST_LIST=!TEST_LIST! %%t
+
+cscript %~dp0..\bin\TestRunner.wsf //Job:ConsoleTestRunner %TEST_LIST%
